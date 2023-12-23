c8e5d074f7f9fde9a82f60ffd3d6d9d659dcde71
evlosh <evlosh@gmail.com>
2023-12-23 20:22:28 +0300

    fit image

--- fit_img.py
+++ fit_img.py
@@ -4,13 +4,26 @@ from PIL import Image
 class FitImg:
     def fit(self, img: Image, w: int, h: int):
         imw, imh = img.size
-        side = min(w, h)
 
-        if imw > imh:
-            delta = side/imw
-            neww, newh = side, int(imh*delta)
+        if -3 < imw - imh < 3:
+            imw, imh = imw, imw
+
+        if w > h:
+
+            if imw > imh:
+                delta = imw/imh
+                neww, newh = int(h*delta), h
+            else:
+                delta = imh/imw
+                neww, newh = int(h/delta), h
+        
         else:
-            delta = side/imh
-            neww, newh = int(imw*delta), side
+
+            if imw > imh:
+                delta = imw/imh
+                neww, newh = w, int(w/delta)
+            else:
+                delta = imh/imw
+                neww, newh = w, int(w*delta)
 
         return img.resize((neww, newh))
