6e618356576ba13f42f64b8de85e2df1eee08c8c
Evgeny Loshkarev <evlosh@gmail.com>
2024-09-19 10:49:19 +0300

    ref

--- fit_img.py
+++ fit_img.py
@@ -2,7 +2,8 @@ from PIL import Image
 
 
 class FitImg:
-    def fit(self, img: Image, w: int, h: int):
+    @staticmethod
+    def fit(img: Image.Image, w: int, h: int) -> Image.Image:
         imw, imh = img.size
 
         if -3 < imw - imh < 3:
@@ -10,32 +11,21 @@ class FitImg:
 
         if w > h:
 
-            print("horizontal window")
-
             if imw > imh:
-                print("horizontal img")
                 delta = imw/imh
                 # neww, newh = int(h*delta), h
                 neww, newh = w, int(w/delta)
             else: # img h > img w
-                print("vertical img")
                 delta = imh/imw
                 neww, newh = int(h/delta), h
         
         else:
 
-            print("vertical window")
-
             if imw > imh:
-                print("horizontal img")
                 delta = imw/imh
                 neww, newh = w, int(w/delta)
             else: # h > w and h > w
-                print("vertival img")
                 delta = imh/imw
                 neww, newh = int(h/delta), h
-
-        print("img", neww, newh)
-        print("widget", w, h)
 
         return img.resize((neww, newh))
--- start.py
+++ start.py
@@ -1,4 +1,6 @@
 import io
+import os
+import subprocess
 import sys
 import threading
 import tkinter
@@ -8,7 +10,8 @@ import tifffile
 from PIL import Image, ImageCms, ImageOps
 from tkinterdnd2 import DND_FILES, TkinterDnD
 
-import subprocess
+from fit_img import FitImg
+
 
 class App:
     def __init__(self):
@@ -40,73 +43,133 @@ class App:
         self.flag = True
 
     def on_stop_click(self, e):
-        print("Стоп кнопка нажата!")
         self.flag = False
 
     def minimize(self, e: tkinter.Event):
         if e.char == "w":
             self.root.wm_withdraw()
 
-    def convert_img(self, src: str):
-        if src.endswith((".tif", ".tiff", ".TIF", ".TIFF")):
+    def convert_tiff(self, src: str):
+        try:
+            img = tifffile.imread(files=src)[:,:,:3]
+            if str(object=img.dtype) != "uint8":
+                img = (img/256).astype(dtype="uint8")
+            img = Image.fromarray(obj=img.astype("uint8"), mode="RGB")
 
+            if img.mode == 'RGBA':
+                img = img.convert('RGB')
+
+            img: Image.Image = FitImg.fit(img, 3000, 3000)
+            # сохраняем джепег
+            save_path = self.new_filename(src)
+            img.save(save_path, "JPEG")
+            self.jpegs.append(save_path)
+
+        except Exception as e:
+            print("tifffle error:", e, src)
+
+    def convert_psd(self, src: str):
+        try:
+            img = Image.open(fp=src)
+            img = ImageOps.exif_transpose(image=img)
+
+            # читаем профиль 
             try:
-                img = tifffile.imread(files=src)[:,:,:3]
-                if str(object=img.dtype) != "uint8":
-                    img = (img/256).astype(dtype="uint8")
-                self.img = Image.fromarray(obj=img.astype("uint8"), mode="RGB")
+                iccProfile = img.info.get('icc_profile')
+                iccBytes = io.BytesIO(iccProfile)
+                icc = ImageCms.ImageCmsProfile(iccBytes)
+                srgb = ImageCms.createProfile('sRGB')
+                img = ImageCms.profileToProfile(img, icc, srgb)
+            except Exception as e:
+                print("icc profile err:", e)
 
-                # сохраняем джепег
-                save_path = f"{src.rsplit('.', 1)[0]}.jpg"
-                self.img.save(save_path, "JPEG")
+            if img.mode == 'RGBA':
+                img = img.convert('RGB')
+
+            img: Image.Image = FitImg.fit(img, 3000, 3000)
+            save_path = self.new_filename(src)
+            img.save(save_path, "JPEG")
+            self.jpegs.append(save_path)
+
+        except Exception as e:
+            print("pillow error:", e, src)
+            # открываем через PSD tools
+            try:
+                img = psd_tools.PSDImage.open(fp=src).composite()
+                save_path = self.new_filename(src)
+
+                if img.mode == 'RGBA':
+                    img = img.convert('RGB')
+
+                img: Image.Image = FitImg.fit(img, 3000, 3000)
+                img.save(save_path, "JPEG")
                 self.jpegs.append(save_path)
-
             except Exception as e:
-                print("tifffle error", e)
-
-        elif src.endswith((".psd", ".PSD", ".psb", ".PSB", ".jpg", ".JPG", ".jpeg", ".JPEG", ".png", ".PNG")):
-
-            # открываем через PIL
-            try:
-                img = Image.open(fp=src)
-                img = ImageOps.exif_transpose(image=img)
-                # читаем профиль 
-                try:
-                    iccProfile = img.info.get('icc_profile')
-                    iccBytes = io.BytesIO(iccProfile)
-                    icc = ImageCms.ImageCmsProfile(iccBytes)
-                    srgb = ImageCms.createProfile('sRGB')
-                    img = ImageCms.profileToProfile(img, icc, srgb)
-                    save_path = f"{src.rsplit('.', 1)[0]}.jpg"
-                    img.save(save_path, "JPEG")
-                    self.jpegs.append(save_path)
-                except Exception as e:
-                    print("icc profile err", e)
-            except Exception as e:
-                print("pillow error", e)
-                # открываем через PSD tools
-                try:
-                    img = psd_tools.PSDImage.open(fp=src).composite()
-                    save_path = f"{src.rsplit('.', 1)[0]}.jpg"
-                    img.save(save_path, "JPEG")
-                    self.jpegs.append(save_path)
-                except Exception as e:
-                    print("psd tools error", e)
+                print("psd tools error:", e, src)
+    
+    def check_ext(self, img: str):
+        img = img.lower()
+        if img.endswith((".tif", ".tiff")):
+            return "tiff"
+        elif img.endswith((".psd", ".psb", ".png")):
+            return "psd"
+        return "other"
+    
+    def new_filename(self, src: str, advanced_name: str="_preview"):
+        return f"{src.rsplit('.', 1)[0]}{advanced_name}.jpg"
 
     def convert_images_list(self, img_list: list):
-        self.flag = True
         self.img_lbl.unbind('<<Drop>>')
-        self.jpegs.clear()
-        ln = len(img_list)
         self.img_lbl.configure(text="Подготовка")
         self.stop_btn.configure(fg="white")
+        self.flag = True
+        self.jpegs.clear()
 
-        for x, img in enumerate(img_list, start=1):
-            if self.flag:
-                self.convert_img(src=img)
-                self.img_lbl.configure(text=f"Конвертирую {x} из {ln}")
+        files_to_convert = {
+            "tiff": [],
+            "psd": [],
+            "other": []
+            }
+
+        for img in img_list:
+            img: str
+
+            if img.endswith(".app"):
+                continue
+
+            if os.path.isfile(img):
+                files_to_convert[self.check_ext(img)].append(img)
+
             else:
+                for root, dirs, files in os.walk(img):
+
+                    if not self.flag:
+                        break
+
+                    for file in files:
+                        img_file = os.path.join(root, file)
+                        files_to_convert[self.check_ext(img_file)].append(img_file)
+
+        ln = len(files_to_convert["tiff"]) + len(files_to_convert["psd"])
+        count = 0
+
+        for tiff_img in files_to_convert["tiff"]:
+
+            if not self.flag:
                 break
+
+            count += 1
+            self.img_lbl.configure(text=f"Конвертирую {count} из {ln}")
+            self.convert_tiff(tiff_img)
+
+        for psd_img in files_to_convert["psd"]:
+
+            if not self.flag:
+                break
+
+            count += 1
+            self.img_lbl.configure(text=f"Конвертирую {count} из {ln}")
+            self.convert_psd(psd_img)
 
         t = ("Перетяните изображения сюда.\n"
              "Поддерживаемые форматы:\n"
